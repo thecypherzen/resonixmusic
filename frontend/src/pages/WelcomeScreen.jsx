88754e7ec065fdb0a0841164fe60c6af6f7e3363
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-19 21:10:14 +0100

    feat: welcome screen

---
+++ frontend/src/pages/WelcomeScreen.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ChevronRight } from 'lucide-react';
+
+const WelcomeScreen = () => {
+  return (
+    <div className="flex flex-col mx-auto gap-6 relative w-screen h-screen">
+
+      {/* gradient blur background */}
+      <div className='w-[600px] h-[600px] bg-gradient-to-r from-purple-500 from-10% via-blue-700 to-sky-500 blur-[200px] rounded-full -z-20 absolute -mt-40 -ml-40 opacity-80 '></div>
+
+      {/* main content */}
+      <div className='flex flex-col items-center gap-8 my-auto'>
+        <p className='text-center text-2xl'>
+          welcome to
+        </p>
+        <img src="/logo-grad.png" alt="resonix logo" className="w-100 mx-auto" />
+
+        {/* next arrow */}
+        <button className='mx-auto bg-[#212121] rounded-full p-2 h-14 w-14 border-neutral-800 border'>
+          <ChevronRight className='mx-auto h-8 w-8' />
+        </button>
+
+        <p className='text-center italic'>
+          ...let the beat resonates in your soul!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomeScreen;
