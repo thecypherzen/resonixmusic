3a89afab0350a3c37004bb4f4028f08d55a8225d
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-19 21:21:05 +0100

    feat: animate gradient bg

--- frontend/src/pages/WelcomeScreen.jsx
+++ frontend/src/pages/WelcomeScreen.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { ChevronRight } from 'lucide-react';
+import { motion } from 'framer-motion';
 
 const WelcomeScreen = () => {
   return (
     <div className="flex flex-col mx-auto gap-6 relative w-screen h-screen">
 
       {/* gradient blur background */}
-      <div className='w-[600px] h-[600px] bg-gradient-to-r from-purple-500 from-10% via-blue-700 to-sky-500 blur-[200px] rounded-full -z-20 absolute -mt-40 -ml-40 opacity-80 '></div>
+      <motion.div className='w-[600px] h-[600px] bg-gradient-to-r from-purple-500 from-10% via-blue-700 to-sky-500 blur-[200px] rounded-full -z-20 absolute -mt-40 -ml-40 opacity-80' animate={{
+        x: [0, 100, -100, 0],
+        y: [0, -100, 100, 0],
+      }}
+        transition={{
+          duration: 20,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut"
+        }}
+        initial={{ x: -200, y: -200 }}>
+      </motion.div>
 
       {/* main content */}
       <div className='flex flex-col items-center gap-8 my-auto'>
