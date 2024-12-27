b209add6cf8dce01bde32213be04fd001661f6ab
William Inyam <sw.inyam@outlook.com>
2024-12-27 20:50:01 +0000

    feat(routes): add playlist by id route

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -1,9 +1,12 @@
 import { Router, json } from 'express';
-import { getTrendingPlaylists } from '../controllers';
+import {
+  getPlaylistById,
+  getTrendingPlaylists,
+} from '../controllers';
 
 const router = Router();
 router.use(json());
 
 router.get('/trending', getTrendingPlaylists);
-
+router.get('/:id', getPlaylistById);
 export default router;
