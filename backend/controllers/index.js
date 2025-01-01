e58ed12830c2283a1137d1c255065d2b28abd783
William Inyam <sw.inyam@outlook.com>
2025-01-01 19:12:51 +0000

    build(controllers): expose tracks controllers

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -5,9 +5,24 @@ import {
   searchPlaylists,
 } from './playListControllers';
 
+import {
+  downloadTrack,
+  getTrackById,
+  getTrackDetails,
+  getTrendingTracks,
+  searchTracks,
+  streamTrack,
+} from './tracksControllers';
+
 export {
+  downloadTrack,
   getPlaylistById,
   getPlaylistTracks,
+  getTrackById,
+  getTrackDetails,
   getTrendingPlaylists,
+  getTrendingTracks,
   searchPlaylists,
+  searchTracks,
+  streamTrack,
 };
