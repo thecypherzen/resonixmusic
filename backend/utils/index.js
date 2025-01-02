cef40e529e32c8085132356c05ff127ca2ec82af
William Inyam <sw.inyam@outlook.com>
2025-01-02 15:35:54 +0000

    feat(utils): expose filters

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,9 +1,12 @@
+import { filterBy, sortBy } from './filters';
 import getPageFromArray from './getPageFrom';
 import globalErrorHandler from './errorHandlers';
 import { requestClient } from './config';
 
 export {
+  filterBy,
   getPageFromArray,
   globalErrorHandler,
   requestClient,
+  sortBy,
 };
