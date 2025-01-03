a3ba403d7b2f3442fe38cc05a8199455e7c43da9
Gabriel Isaac <stewarti3@yahoo.com>
2025-01-03 18:06:16 +0100

    update: searchbar position

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -6,7 +6,7 @@ const TopNav = () => {
   return (
     <div className='flex flex-row bg-transparent items-center py-10 px-16 h-[4.375rem] w-full mx-auto sticky top-0 z-50 bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20'>
       {/* Searchbar */}
-      <div className='flex bg-[#212121] h-[2.5rem] rounded-xl border border-neutral-800 m-4 items-center p-4 transition-all duration-300 hover:w-[40rem] focus-within:w-[40rem] w-[30rem]'>
+      <div className='flex bg-[#212121] h-[2.5rem] rounded-xl border border-neutral-800 my-4 items-center p-4 transition-all duration-300 hover:w-[40rem] focus-within:w-[40rem] w-[30rem]'>
         <LuSearch className='w-4 h-4' />
         <input
           type='search'
