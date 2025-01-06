520bcaf77f093f184f92f3b58a0c654854cb48b8
William Inyam <sw.inyam@outlook.com>
2025-01-06 15:05:54 +0000

    build(utils): add cache client

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -2,8 +2,11 @@ import { filterBy, sortBy } from './filters';
 import getPageFromArray from './getPageFrom';
 import globalErrorHandler from './errorHandlers';
 import { requestClient } from './config';
+import { cacheClient, cacheClientReady } from './cacheClient';
 
 export {
+  cacheClient,
+  cacheClientReady,
   filterBy,
   getPageFromArray,
   globalErrorHandler,
