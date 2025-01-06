5475c13b88ab4c2673945265d0add5d08e2571fb
Gabriel Isaac <stewarti3@yahoo.com>
2025-01-06 15:53:17 +0100

    update: nav links

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, House, Compass, ChartNoAxesCombined, ListMusic, Disc, Music, MicVocal, Plus, Trash2, X } from 'lucide-react';
+import { Menu, House, Compass, ChartNoAxesCombined, ListMusic, Library, Music, Download, Plus, Trash2, X } from 'lucide-react';
+import { FaPlay } from 'react-icons/fa6';
+import { usePlayer } from '../context/PlayerContext';
 
 // Create a new CreatePlaylistModal component
 const CreatePlaylistModal = ({ isOpen, onClose, onSubmit }) => {
@@ -74,6 +76,7 @@ const SideBar = () => {
   const userLogin = 'gabrielisaacs';
   const initials = userLogin.substring(0, 2).toUpperCase();
   const location = useLocation();
+  const { play } = usePlayer();
 
   const handleCreatePlaylist = (playlistData) => {
     const newPlaylist = {
@@ -88,24 +91,31 @@ const SideBar = () => {
     setIsModalOpen(false);
   };
 
-  const handleDeletePlaylist = (indexToDelete) => {
-    setPlaylists(playlists.filter((_, index) => index !== indexToDelete));
+  // const handleDeletePlaylist = (indexToDelete) => {
+  //   setPlaylists(playlists.filter((_, index) => index !== indexToDelete));
+  // };
+
+  const handlePlayAllSongs = (playlist) => {
+    if (playlist.tracks.length > 0) {
+      play(playlist.tracks[0], playlist.tracks); // Play the first track and set the queue
+    }
   };
+
 
   return (
     <>
-      <div className='fixed left-0 top-0 bottom-0 w-[15rem] flex flex-col h-screen overflow-hidden bg-[#212124] bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 z-40'>
+      <div className='fixed left-0 top-0 bottom-0 w-[15rem] flex flex-col h-screen overflow-hidden bg-[#212124] bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 z-40'>
         <div className='px-6 py-6'>
           {/* Navbar */}
           <div className="w-full h-[4.375rem]">
-            <div className="flex flex-row gap-4 items-center">
+            <div className="flex flex-row gap-2 items-center">
               {/* Profile */}
-              <button className="flex w-[2rem] h-[2rem] bg-[#212124] rounded-full border border-neutral-800 items-start">
-                <p className="text-white text-xs text-center justify-center m-auto shadow-lg hover:opacity-60 transition-opacity duration-200">{initials}</p>
-              </button>
+              <img src="/logo-grad.png" alt="resonix logo" className='w-[5.75rem] h-auto' />
 
-              <img src="/logo-grad.png" alt="resonix logo" className='w-[5.313rem] h-[0.875rem]' />
-              <button className='ml-auto bg-transparent'>
+              <button className="flex w-[2rem] h-[2rem] bg-[#212124] rounded-full border border-neutral-800 items-start ml-auto">
+                <p className="text-white text-xs text-center justify-center m-auto shadow-lg hover:opacity-60 transition-opacity duration-200 ">{initials}</p>
+              </button>
+              <button className='bg-transparent'>
                 <Menu />
               </button>
             </div>
@@ -131,54 +141,23 @@ const SideBar = () => {
               <p>Explore</p>
             </Link>
             <Link
-              to='/trending'
+              to='/playlist'
               className={`inline-flex gap-2 text-white hover:text-[#08B2F0] text-base transition-colors duration-200 ${location.pathname === '/trending' ? 'text-[#08B2F0]' : ''
                 }`}
               onClick={(e) => e.preventDefault()}
             >
-              <ChartNoAxesCombined />
-              <p>Trending</p>
+              <Library />
+              <p>Library</p>
             </Link>
-          </div>
-
-          {/* Collection Section */}
-          <div className="flex flex-col gap-6 my-6 text-[0.875rem]">
-            <p className="text-white opacity-40 text-xs font-400 -mb-2">MY COLLECTION</p>
-            <div className="flex flex-col ml-2 space-y-6">
-              <Link
-                to="/playlists"
-                className="inline-flex gap-2 hover:text-[#08B2F0] transition-colors duration-200"
-                onClick={(e) => e.preventDefault()}
-              >
-                <ListMusic />
-                <p>Playlist</p>
-              </Link>
-              <Link
-                to="/albums"
-                className="inline-flex gap-2 hover:text-[#08B2F0] transition-colors duration-200"
-                onClick={(e) => e.preventDefault()}
-              >
-                <Disc />
-                <p>Albums</p>
-              </Link>
-              <Link
-                to="/artists"
-                className="inline-flex gap-2 hover:text-[#08B2F0] transition-colors duration-200"
-                onClick={(e) => e.preventDefault()}
-              >
-                <MicVocal />
-                <p>Artists</p>
-              </Link>
-            </div>
           </div>
 
           {/* Playlists Section */}
           <div className="flex flex-col gap-4 my-6 text-[0.875rem] pb-[7rem] overflow-y-auto">
             <p className="text-white opacity-40 text-xs font-400">MY PLAYLISTS</p>
-            <div className="flex flex-col gap- group">
+            <div className="flex flex-col">
               <button
                 onClick={() => setIsModalOpen(true)}
-                className="inline-flex items-center justify-center py-2 px-4 bg-transparent gap-1 group-hover:bg-[#212121] transition-all duration-200 rounded-full border border-neutral-700 w-full"
+                className="inline-flex items-center justify-center py-2 px-4 bg-transparent gap-1 hover:bg-[#212121] transition-all duration-200 rounded-full border border-neutral-700 w-full"
               >
                 <Plus size={16} />
                 <span className="text-xs">Create Playlist</span>
@@ -198,10 +177,10 @@ const SideBar = () => {
                         {playlist.title}
                       </Link>
                       <button
-                        onClick={() => handleDeletePlaylist(index)}
-                        className="opacity-0 group-hover:opacity-100 transition-all duration-200 ml-2 bg-transparent hover:text-red-500"
+                        onClick={() => handlePlayAllSongs(playlist)}
+                        className="opacity-0 group-hover:opacity-100 transition-all duration-200 ml-2 bg-transparent"
                       >
-                        <Trash2 size={16} />
+                        <FaPlay size={16} />
                       </button>
                     </div>
                   ))}
