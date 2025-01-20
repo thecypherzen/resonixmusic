dd428b46e4bb0b8c460ac1de7d1cfcd6b998a094
Gabriel Isaac <stewarti3@yahoo.com>
2025-01-20 13:44:52 +0100

    update: components position

--- frontend/src/pages/AlbumDetailsPage.jsx
+++ frontend/src/pages/AlbumDetailsPage.jsx
@@ -9,14 +9,28 @@ const AlbumDetailsPage = () => {
   const { id } = useParams();
 
   return (
-    <div className="flex min-h-screen w-screen bg-[#121212]">
-      <SideBar />
-      <div className="flex-1 ml-[14rem]">
-        <TopNav />
-        <main className="pb-[7rem]">
+    <div className="flex min-h-screen w-screen bg-[#121212] relative">
+      {/* Sidebar */}
+      <div className="fixed left-0 top-0 h-[calc(100vh-90px)] w-[14rem] z-30">
+        <SideBar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 ml-[14rem] relative">
+        {/* Top Navigation - Fixed position */}
+        <div className="fixed top-0 right-0 left-[14rem] h-[4rem] z-20 bg-[#121212]">
+          <TopNav />
+        </div>
+
+        {/* Main Content Area */}
+        <div className="mt-[4rem] pb-[90px]">
           <AlbumDetails id={id} />
-        </main>
-        <BottomPlayer />
+        </div>
+
+        {/* Bottom Player */}
+        <div className="fixed bottom-0 left-0 right-0 h-[90px] z-50">
+          <BottomPlayer />
+        </div>
       </div>
     </div>
   );
