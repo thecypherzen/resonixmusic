c480c4d9de9c18f6de8b462fdc41a97d9c6041e4
William Inyam <sw.inyam@outlook.com>
2025-01-22 11:44:02 +0000

    feat(artists): implement getArtistsAlbums route and controller

--- backend/controllers/artists/index.js
+++ backend/controllers/artists/index.js
@@ -1,7 +1,9 @@
 import getArtists from './getArtists.js';
 import getArtistsTracks from './getTracks.js';
+import getArtistsAlbums from './getAlbums.js'
 
 export {
   getArtists,
+  getArtistsAlbums,
   getArtistsTracks,
 };
