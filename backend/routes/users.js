3c9760150142da1195ab38732d35fae81c52bf3b
William Inyam <sw.inyam@outlook.com>
2025-01-22 13:59:30 +0000

    build(users): add getUsers controller, router and swagger docs

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -29,9 +29,156 @@ router.get(
   '/tracks',
   getUsersTracks
 );
-
+/**
+ * @openapi
+ *   /users:
+ *     get:
+ *       tags:
+ *         - Get Users
+ *       summary: Get information of a set of Users
+ *       description: |
+ *         Requires at least one of the following parameters
+ *         `id`, `access_token`, and `name`.
+ *       operationId: getUsers
+ *       parameters:
+ *         - in: query
+ *           name: access_token
+ *           description: |
+ *             A valid access token corresponding to the Jamendo user
+ *             you want to get data for.
+ *           schema:
+ *             type: string
+ *         - in: query
+ *           name: format
+ *           description: |
+ *             The way to format the response data body
+ *           schema:
+ *             $ref: '#/components/schemas/format'
+ *         - in: query
+ *           name: full_count
+ *           description: |
+ *             Sets the 'results_fullcount' value in the results
+ *             data 'headers' to the total number results(tracks)
+ *             in the database. Useful for pagination purposes.
+ *           schema:
+ *             $ref: '#/components/schemas/results_full_count'
+ *         - in: query
+ *           name: id
+ *           description: One or more user IDs.
+ *           schema:
+ *             $ref: '#/components/schemas/TrackIds'
+ *         - in: query
+ *           name: image_size
+ *           description: |
+ *             The size of the cover of the returned resource.
+ *             A size of n returns the 'nxn-sized' cover image.
+ *           schema:
+ *             $ref: '#/components/schemas/user_image_size'
+ *         - in: query
+ *           name: name
+ *           description: The User's name
+ *           schema:
+ *             type: string
+ *         - in: query
+ *           name: order_by
+ *           description: |
+ *             Sort results by a given field. Can also specify
+ *             if order should be ascending or descending by
+ *             adding `_asc` or `_desc` resppectively. The default
+ *             order is ascending. Supported values are
+ *             `access_token`, `id`, and `name`. By default, they
+ *             are sorted by relevance.
+ *           schema:
+ *             type: string
+ *             enum:
+ *               - access_token
+ *               - id
+ *               - name
+ *         - in: query
+ *           name: page
+ *           description: The page number to return
+ *           schema:
+ *             $ref: '#/components/schemas/page'
+ *         - in: query
+ *           name: page_size
+ *           description: The number of playlists in a single
+ *             result list
+ *           schema:
+ *             $ref: '#/components/schemas/page_size'
+ */
 router.get(
   '/',
+  [
+    query([
+      'access_token', 'name'
+    ])
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .escape(),
+    query('format')
+      .optional()
+      .trim()
+      .isIn(['json', 'jsonpretty'])
+      .withMessage('Expects json or jsonpretty')
+      .escape(),
+    query('full_count')
+      .optional()
+      .trim()
+      .toBoolean()
+      .isBoolean({ strict: true })
+      .withMessage('Expects true/false')
+      .escape(),
+    query('id')
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isArray()
+      .withMessage('Expects an array')
+      .escape(),
+    query('id.*')
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: 1 })
+      .withMessage('Expects an integer > 0')
+      .escape(),
+    query('image_size')
+      .default('100')
+      .trim()
+      .isInt()
+      .withMessage('Expects an integer > 0')
+      .isIn([ 30, 50, 100 ])
+      .withMessage('Invalid image_size. See docs at /docs'),
+    query('order_by')
+      .optional()
+      .default('id_asc')
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isIn([
+        'access_token', 'access_token_asc', 'access_token_desc',
+        ,'id', 'id_asc', 'id_desc', 'name', 'name_asc', 'name_desc',
+      ])
+      .escape(),
+    query('page')
+      .default('1')
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: 1 })
+      .withMessage('Expects an integer > 0')
+      .escape(),
+    query('page_size')
+      .default(`${MIN_PAGE_SIZE}`)
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: MIN_PAGE_SIZE, max: MAX_PAGE_SIZE })
+      .withMessage(`Expects an integer from ${MIN_PAGE_SIZE}`
+                  + ` to ${MAX_PAGE_SIZE}`)
+      .escape(),
+  ],
   getUsers
 );
 
