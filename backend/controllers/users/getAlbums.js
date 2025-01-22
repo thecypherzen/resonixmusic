a881f24312432ad154fae5a6d63ad489c6346b92
William Inyam <sw.inyam@outlook.com>
2025-01-22 16:25:03 +0000

    feat: implement getUsersAlbums controller and router

--- backend/controllers/users/getAlbums.js
+++ backend/controllers/users/getAlbums.js
@@ -1,5 +1,56 @@
+import {
+  matchedData,
+  validationResult,
+} from 'express-validator';
+
+import {
+  errorHandlers as handlers,
+  requestClient,
+  RequestClientError,
+} from '../../utils/index.js';
+
+import {
+  RESPONSE_CODES as resCodes,
+  REQPARAMS as defaultParams,
+} from '../../defaults/index.js';
+
 async function getAlbums(req, res) {
-  return res.send({ controller: 'getUsersAlbums' });
+  // get albums
+  const validation = validationResult(req);
+
+  // verify no validation error
+  if (!validation.isEmpty()) {
+    return handlers.validationError(validation.array(), res);
+  }
+
+  const config = {
+    url: `/users/albums`,
+    params: { ...defaultParams },
+  };
+
+  const queryParams = matchedData(req, { locations: ['query'] });
+  requestClient.setQueryParams(queryParams, config);
+  // return res.send(config);
+  let resData = null;
+  try {
+    const response = await requestClient.make(config);
+    // set response.data.headers
+    requestClient.setDataHeaders(response.data, {
+      options: { 'x-took': response.timeTaken },
+    });
+    resData = response.data;
+  } catch (error) {
+    resData = {};
+    // set headers in resData
+    requestClient.setResStatus(error.code, res);
+    // req.res = res; [TEMPORAL - uncomment after check]
+    requestClient.setDataHeaders(resData, {
+      error, options: {'x-took': error.timeTaken },
+       });
+  } finally {
+    requestClient.log({ req });
+    return res.send(resData);
+  }
 }
 
 export default getAlbums;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -15,8 +15,170 @@ import {
 const router = Router();
 router.use(json());
 
+/**
+ * @openapi
+ *   /users/albums:
+ *     get:
+ *       tags:
+ *         - Get Users Albums
+ *       summary: Get albums of one or more users
+ *       description: |
+ *         Requires at least one of the following parameters
+ *         `id`, `access_token`, and `name`.
+ *       operationId: getUsers
+ *       parameters:
+ *         - in: query
+ *           name: album_id
+ *           description: One or more album IDs
+ *           schema:
+ *             $ref: '#/components/schemas/TrackIds'
+ *         - in: query
+ *           name: access_token
+ *           description: |
+ *             A valid access token corresponding to the Jamendo user
+ *             you want to get data for.
+ *           schema:
+ *             type: string
+ *         - in: query
+ *           name: album_imagesize
+ *           description: The cover size of album in pixels
+ *           schema:
+ *             $ref: '#/components/schemas/image_size'
+ *         - in: query
+ *           name: format
+ *           description: |
+ *             The way to format the response data body
+ *           schema:
+ *             $ref: '#/components/schemas/format'
+ *         - in: query
+ *           name: full_count
+ *           description: |
+ *             Sets the 'results_fullcount' value in the results
+ *             data 'headers' to the total number results(tracks)
+ *             in the database. Useful for pagination purposes.
+ *           schema:
+ *             $ref: '#/components/schemas/results_full_count'
+ *         - in: query
+ *           name: id
+ *           description: One or more user IDs.
+ *           schema:
+ *             $ref: '#/components/schemas/TrackIds'
+ *         - in: query
+ *           name: image_size
+ *           description: |
+ *             The size of the avatar of the user in pixels
+ *           schema:
+ *             $ref: '#/components/schemas/user_image_size'
+ *         - in: query
+ *           name: name
+ *           description: The User's name
+ *           schema:
+ *             type: string
+ *         - in: query
+ *           name: order_by
+ *           description: |
+ *             Sort results by `createdate`. Can also specify
+ *             if order should be ascending or descending by
+ *             adding `_asc` or `_desc` resppectively. The default
+ *             order is ascending.
+ *           schema:
+ *             type: string
+ *             enum:
+ *               - createdate
+ *         - in: query
+ *           name: page
+ *           description: The page number to return
+ *           schema:
+ *             $ref: '#/components/schemas/page'
+ *         - in: query
+ *           name: page_size
+ *           description: The number of playlists in a single
+ *             result list
+ *           schema:
+ *             $ref: '#/components/schemas/page_size'
+ */
 router.get(
   '/albums',
+  [
+    query([
+      'access_token', 'name'
+    ])
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .escape(),
+    query('format')
+      .optional()
+      .trim()
+      .isIn(['json', 'jsonpretty'])
+      .withMessage('Expects json or jsonpretty')
+      .escape(),
+    query('full_count')
+      .optional()
+      .trim()
+      .toBoolean()
+      .isBoolean({ strict: true })
+      .withMessage('Expects true/false')
+      .escape(),
+    query('album_id', 'id')
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isArray()
+      .withMessage('Expects an array')
+      .escape(),
+    query('album_id.*', 'id.*')
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: 1 })
+      .withMessage('Expects an integer > 0')
+      .escape(),
+    query('album_imagesize')
+      .default('500')
+      .trim()
+      .isInt()
+      .withMessage('Expects an integer > 0')
+      .isIn([
+        25, 35, 50, 55, 60, 65, 70, 75, 85, 100, 130,
+        150, 200, 300, 400, 500, 600
+      ])
+      .withMessage('Invalid image_size. See docs at /docs'),
+    query('image_size')
+      .default('100')
+      .trim()
+      .isInt()
+      .withMessage('Expects an integer > 0')
+      .isIn([ 30, 50, 100 ])
+      .withMessage('Invalid image_size. Expects 30, 50 or 100'),
+    query('order_by')
+      .optional()
+      .default('id_asc')
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isIn([
+        'updatedate', 'updatedate_asc', 'updatedate_desc'
+      ])
+      .escape(),
+    query('page')
+      .default('1')
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: 1 })
+      .withMessage('Expects an integer > 0')
+      .escape(),
+    query('page_size')
+      .default(`${MIN_PAGE_SIZE}`)
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: MIN_PAGE_SIZE, max: MAX_PAGE_SIZE })
+      .withMessage(`Expects an integer from ${MIN_PAGE_SIZE}`
+                  + ` to ${MAX_PAGE_SIZE}`)
+      .escape(),
+  ],
   getUsersAlbums
 );
 
@@ -29,6 +191,7 @@ router.get(
   '/tracks',
   getUsersTracks
 );
+
 /**
  * @openapi
  *   /users:
@@ -86,14 +249,12 @@ router.get(
  *             if order should be ascending or descending by
  *             adding `_asc` or `_desc` resppectively. The default
  *             order is ascending. Supported values are
- *             `access_token`, `id`, and `name`. By default, they
- *             are sorted by relevance.
+ *             `updatedate`. By default, they are sorted
+ *             by relevance.
  *           schema:
  *             type: string
  *             enum:
- *               - access_token
- *               - id
- *               - name
+ *               - updatedate
  *         - in: query
  *           name: page
  *           description: The page number to return
@@ -150,7 +311,7 @@ router.get(
       .isInt()
       .withMessage('Expects an integer > 0')
       .isIn([ 30, 50, 100 ])
-      .withMessage('Invalid image_size. See docs at /docs'),
+      .withMessage('Invalid image_size. Expects 30, 50 or 100'),
     query('order_by')
       .optional()
       .default('id_asc')
@@ -158,8 +319,7 @@ router.get(
       .notEmpty()
       .withMessage('Value cannot be empty')
       .isIn([
-        'access_token', 'access_token_asc', 'access_token_desc',
-        ,'id', 'id_asc', 'id_desc', 'name', 'name_asc', 'name_desc',
+        'updatedate', 'updatedate_asc', 'updatedate_desc'
       ])
       .escape(),
     query('page')
