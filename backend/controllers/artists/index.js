423df1c93353458d213e2d3b6b8eca87307fe90b
William Inyam <sw.inyam@outlook.com>
2025-01-22 11:14:22 +0000

    feat(artists): implement route, and controller for getArtistsTracks

---
+++ backend/controllers/artists/index.js
@@ -0,0 +1,7 @@
+import getArtists from './getArtists.js';
+import getArtistsTracks from './getTracks.js';
+
+export {
+  getArtists,
+  getArtistsTracks,
+};
--- backend/routes/artists.js
+++ backend/routes/artists.js
@@ -2,6 +2,7 @@ import { Router, json } from 'express';
 import { query } from 'express-validator';
 import {
   getArtists,
+  getArtistsTracks,
 } from '../controllers/index.js';
 import {
   MAX_PAGE_SIZE,
@@ -10,6 +11,248 @@ import {
 
 const router = Router();
 router.use(json());
+
+
+/**
+ * @openapi
+ * /artists/tracks:
+ *   get:
+ *     tags:
+ *       - Get Artists Tracks
+ *     summmary: Get tracks by one or more artists
+ *     description: |
+ *       By default, tracks returned are sorted in descending order
+ *       of weekly popularity of the artist - using the parameter
+ *       `popularity_week_desc`. Order can be changed by passing
+ *       a different value for the `order_by` parameter.
+ *     parameters:
+ *       - in: query
+ *         name: album_datebetween
+ *         description: |
+ *           Dates between which the artist(s)'s album is
+ *           created. Format is same as `date_between` parameter.
+ *       - in: query
+ *         name: album_id
+ *         description: |
+ *           One or more album IDs. Use if you want tracks by an
+ *           artist in particular album (or albums). Useful when, for
+ *           instance a user clicks play on a user's album.
+ *         schema:
+ *           $ref: '#/components/schemas/AlbumIds'
+ *       - in: query
+ *         name: album_name
+ *         description: An album name. An album created by the artist.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: audio_format
+ *         description: |
+ *           The audio format you wish to use on the `fileurl`
+ *           returned field - that is the format of the track
+ *           that would be accessed via the `fileurl` field.
+ *           Supported values are `mp31`, `mp32`, `ogg` and `flac`
+ *           Default is `mp32`.
+ *         schema:
+ *           $ref: '#/components/schemas/track_audio_format'
+ *         example: audio_format=flac
+ *       - in: query
+ *         name: date_between
+ *         description: |
+ *           A filter of artists by their join date. Consists of
+ *           'from' and 'to' fields separated by a '_'. Each
+ *           field must be in teh format 'yyyy-mm-dd'.
+ *         example: date_between=2025-01-12_2019-01-10
+ *         schema:
+ *           $ref: '#/components/schemas/date_between'
+ *       - in: query
+ *         name: format
+ *         description: The way to format the response data body
+ *         schema:
+ *           $ref: '#/components/schemas/format'
+ *       - in: query
+ *         name: full_count
+ *         description: |
+ *           Sets the 'results_fullcount' value in the results data
+ *           'headers' to the total number results(tracks) in the
+ *           database. Useful for pagination purposes. Use only when
+ *           is really necessary due to it's performance issues.
+ *         schema:
+ *           $ref: '#/components/schemas/results_full_count'
+ *       - in: query
+ *         name: has_image
+ *         description: |
+ *           A filter that selects only artist tracks with an image.
+ *           Default value is `true`.
+ *         schema:
+ *           type: boolean
+ *           default: true
+ *       - in: query
+ *         name: id
+ *         description: One or more track IDs.
+ *         schema:
+ *           $ref: '#/components/schemas/TrackIds'
+ *       - in: query
+ *         name: image_size
+ *         description: |
+ *           The size of the cover of the returned resource.
+ *           A size of n returns the 'nxn-sized' cover image.
+ *         schema:
+ *           $ref: '#/components/schemas/image_size'
+ *       - in: query
+ *         name: name
+ *         description: An artist's name.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: name_search
+ *         description: |
+ *           Search for artists that contain all or part of the
+ *           passed value in their name. That is a match against
+ *           `name=*value*`.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: order_by
+ *         description: |
+ *           Sort results by a given field. Can also specify if
+ *           the order should be ascending or descending by adding
+ *           `_asc` or `_desc` resppectively. The default order
+ *           is ascending. Supported values are: `id`, `joindate`,
+ *           `name`, `popularity_month`, `popularity_total`,
+ *           `popularity_week`.
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - id
+ *             - joindate
+ *             - name
+ *             - popularity_month
+ *             - popularity_total
+ *             - popularity_week
+ *       - in: query
+ *         name: page_size
+ *         description: The number of tracks in a single result list
+ *         schema:
+ *           $ref: '#/components/schemas/page_size'
+ *       - in: query
+ *         name: page
+ *         description: The page number to return
+ *         schema:
+ *            $ref: '#/components/schemas/page'
+ *       - in: query
+ *         name: track_id
+ *         description: One or more track id
+ *         schema:
+ *           $ref: #/components/schemas/TrackIds
+ *       - in: query
+ *         name: track_name
+ *         description: Name of track to fetch
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: track_type
+ *         description:
+ */
+router.use(
+  '/tracks',
+  [
+    query(['album_datebetween', 'date_between'])
+      .optional()
+      .trim()
+      .matches(
+        /^([0-9]{4})-([0-9]{2})-([0-9]{2})_([0-9]{4})-([0-9]{2})-([0-9]{2})$/
+      )
+      .withMessage('Expects format: yyyy-mm-dd_yyy-mm-dd'),
+    query(['album_id', 'id', 'track_id', 'track_type'])
+      .notEmpty()
+      .trim()
+      .withMessage('Value cannot be empty')
+      .escape(),
+    query(['album_id.*', 'id.*', 'track_id.*'])
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: 1 })
+      .withMessage('Expects an integer > 0')
+      .escape(),
+    query(['album_name', 'name', 'name_search', 'track_name'])
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .escape(),
+    query('audio_format')
+     .optional()
+     .trim()
+     .isIn(['mp31', 'mp32', 'ogg', 'flac'])
+     .withMessage('Expects mp31, mp32, ogg or flac'),
+    query('format')
+      .optional()
+      .trim()
+      .isIn(['json', 'jsonpretty'])
+      .withMessage('Expects json or jsonpretty'),
+    query('full_count')
+    .optional()
+    .trim()
+    .toBoolean()
+    .isBoolean({ strict: true })
+    .withMessage('Expects true/false'),
+    query('has_image')
+      .default('true')
+      .trim()
+      .toBoolean()
+      .isBoolean({ strict: true })
+      .withMessage('Expects true/false')
+      .escape(),
+    query('image_size')
+      .optional()
+      .trim()
+      .isIn([
+        20, 35, 50, 55, 60, 65, 70, 75, 85,
+        100, 130, 150, 200, 300, 400, 500, 600
+      ])
+      .withMessage('Invalid image_size. See docs at /docs'),
+    query('order_by')
+      .default('popularity_week_desc')
+      .trim()
+      .isIn([
+        'id', 'id_asc', 'id_desc', 'joindate', 'joindate_asc',
+        'joindate_desc', 'name', 'name_asc', 'name_desc',
+        'popularity_month', 'popularity_month_asc',
+        'popularity_month_desc', 'popularity_total',
+        'popularity_total_asc', 'popularity_total_desc',
+        'popularity_week', 'popularity_week_asc',
+        'popularity_week_desc', 'track_id', 'track_id_asc',
+        'track_id_desc', 'track_name', 'track_name_asc',
+        'track_name_desc', 'track_releasedate',
+        'track_releasedate_asc', 'track_releasedate_desc'
+      ])
+      .withMessage('Invalid order_by. See docs at /docs'),
+    query('page')
+      .default('1')
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: 1 })
+      .withMessage('Expects an integer > 0')
+      .escape(),
+     query('page_size')
+      .default(`${MIN_PAGE_SIZE}`)
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isInt({ min: MIN_PAGE_SIZE, max: MAX_PAGE_SIZE })
+      .withMessage(`Expects an integer from ${MIN_PAGE_SIZE}`
+                  + ` to ${MAX_PAGE_SIZE}`)
+      .escape(),
+    query('track_type.*')
+      .trim()
+      .notEmpty()
+      .withMessage('Value cannot be empty')
+      .isIn(['albumtrack', 'single'])
+      .withMessage('Expects albumtrack or single')
+      .escape(),
+  ],
+  getArtistsTracks);
 
 /**
  * @openapi
