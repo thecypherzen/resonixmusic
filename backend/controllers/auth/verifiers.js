ee61f292a158b9e0f68da12162bc1471c663011a
William Inyam <sw.inyam@outlook.com>
2025-01-24 13:01:35 +0000

    fix(auth): resolve login and logout verifier logics

--- backend/controllers/auth/verifiers.js
+++ backend/controllers/auth/verifiers.js
@@ -1,14 +1,34 @@
+import {
+  authClient
+} from '../../utils/index.js';
+
 async function isLoggedIn(req) {
   // verify client is logged in
   const { access_token = null } = req.cookies;
-  return access_token !== null;
+  if (!access_token) {
+    return false;
+  }
+  const {
+    expires_in,
+    refresh_token
+  } = await authClient.getTokenData(access_token);
+  return expires_in > -2;
+
 }
 
 async function isLoggedOut(req) {
   // verify client is logged out
   const { access_token = null } = req.cookies;
-  return access_token === null;
+  if (!access_token) {
+    return true;
+  }
+  const {
+    expires_in,
+    refresh_token
+  } = await authClient.getTokenData(access_token);
+  return expires_in === -2;
 }
+
 
 const verifiers = {
   isLoggedIn,
