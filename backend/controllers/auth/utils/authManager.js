d4136c1ed8f1a02acb38384aa2b7b4e746f0cb41
William Inyam <sw.inyam@outlook.com>
2025-01-24 02:28:43 +0000

    build(auth): create authManager module

---
+++ backend/controllers/auth/utils/authManager.js
@@ -0,0 +1,31 @@
+import {
+  authClient,
+} from '../../../utils/index.js';
+
+async function sendData(req, res) {
+  const { access_token } = req.cookies;
+  const resData = { headers: {}, results: [] };
+  try {
+    const time = Date.now();
+    const authData = await authClient.getTokenData(access_token);
+    authClient.setDataHeaders(resData, {
+      options: {
+        'x-took': `${Date.now() - time}ms`,
+        error_message: 'user already logged in',
+      },
+    });
+    resData.results.push(authData);
+    return res.send(resData);
+  } catch (error) {
+    error.code = error?.code ?? 500;
+    error.timeTaken = `${Date.now() - time}ms`;
+    authClient.setDataHeaders(resData, { error: error })
+    return res.send(resData);
+  }
+}
+
+const manager = {
+  sendData,
+};
+
+export default manager;
