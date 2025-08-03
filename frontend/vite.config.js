e7f96509bc2752524be6253fd51be3f4dcfc608a
William Inyam <sw.inyam@outlook.com>
2025-08-03 14:21:18 +0000

    :up: update: add shadcn and upgrade tailwindcss to v4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
+import path from "path";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 8082,
     host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 }));
