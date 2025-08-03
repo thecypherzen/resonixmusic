f339a9c5bb99233ee37766e98e490c6c64de5d0b
William Inyam <sw.inyam@outlook.com>
2025-08-03 21:23:57 +0000

    :up: update(app): add theme provider to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
-import { PlayerProvider } from './context/PlayerContext';
-import ErrorBoundary from './components/ErrorBoundary';
-import ScrollToTop from './components/ScrollToTop';
-import Routes from './routes';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from "react-router-dom";
+import { PlayerProvider } from "./context/PlayerContext";
+import ErrorBoundary from "./components/ErrorBoundary";
+import ScrollToTop from "./components/ScrollToTop";
+import Routes from "./routes";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./hooks/useTheme";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <ErrorBoundary>
-          <PlayerProvider>
-            <ScrollToTop />
-            <Routes />
-          </PlayerProvider>
+          <ThemeProvider>
+            <PlayerProvider>
+              <ScrollToTop />
+              <Routes />
+            </PlayerProvider>
+          </ThemeProvider>
         </ErrorBoundary>
       </AuthProvider>
     </BrowserRouter>
