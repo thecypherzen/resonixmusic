bdad84751f552fe98f415bad212ee78aaa71497a
William Inyam <sw.inyam@outlook.com>
2025-08-04 11:14:09 +0000

    :up: update: cleanup code into a repo

---
+++ frontend/src/reusables.js
@@ -0,0 +1,18 @@
+// Function to handle sticky navigation
+//const stickyScroll = () => {
+//  if (sideNav) {
+//    if (sideNav.getBoundingClientRect().top <= 0) {
+//      sideNav.classList.add("sticky-nav-header");
+//    } else {
+//      sideNav.classList.remove("sticky-nav-header");
+//    }
+//  }
+//};
+
+//setSideNav(document.getElementById("sticky-nav-header"));
+//if (!isMobileBreakpoint && sideNav) {
+//  window.addEventListener("scroll", stickyScroll);
+//  return () => {
+//    window.removeEventListener("scroll", stickyScroll);
+//  };
+//}
