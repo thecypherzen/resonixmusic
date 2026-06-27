f7b5307fb6c4c03b47f7b4b0cbb1adc9c8e2059d
William Inyam <sw.inyam@outlook.com>
2026-06-27 22:52:06 +0100

    :lipstick:feat:enable tracks selection with btn selections

--- frontend/src/components/TracksList.jsx
+++ frontend/src/components/TracksList.jsx
@@ -1,103 +1,166 @@
+import { useState } from "react";
+import { useIsMedia } from "@/hooks/useIsMobile";
 import UsePlayer from "@/hooks/UsePlayer";
-import { formatDuration } from "@/lib/utils";
-import { FaPlay, FaPause, FaDownload, FaClock } from "react-icons/fa";
+import { cn, formatDuration } from "@/lib/utils";
+import { EllipsisVerticalIcon } from "lucide-react";
+import {
+  FaPlay,
+  FaPause,
+  FaDownload,
+  FaThumbsUp,
+  FaRegThumbsDown,
+  FaRegThumbsUp,
+  FaThumbsDown,
+} from "react-icons/fa";
 
 export default function TracksList({ tracks = null }) {
+  const [activeTrack, setActiveTrack] = useState("");
+  const isMd = useIsMedia(768);
   const { isPlaying, currentTrack, isLoading } = UsePlayer();
+  const toggleTrackSelection = (p) => {
+    if (activeTrack && activeTrack === p) {
+      setActiveTrack("");
+      return;
+    }
+    setActiveTrack(p);
+  };
   if (!tracks || tracks.length === 0) {
     return <p className="text-center">No tracks available.</p>;
   }
   return (
-    <div className="px-6 pb-36 overflow-x-scroll min-h-screen">
-      <table className="w-full h-full">
-        <thead>
-          <tr className="text-sm text-neutral-400 border-b border-neutral-800 transition-all duration-300">
-            <th className="px-4 py-2 text-left w-12">#</th>
-            <th className="px-4 py-2 text-left">Title</th>
-            <th className="px-4 py-2 text-left">Plays</th>
-            <th className="px-4 py-2 text-center">Download</th>
-            <th className="px-4 py-2 text-right">
-              <FaClock className="ml-auto mr-2.5" />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {tracks.map((track, index) => {
-            if (!index) {
-              console.log(track);
-            }
-            return (
-              <tr
-                key={track.id}
-                className="group hover:bg-neutral-800/50 rounded-lg transition-colors cursor-pointer"
-                onClick={() => {}}
-              >
-                <td className="px-4 py-3 text-neutral-400 w-12">
-                  <div className="relative w-4">
-                    {isPlaying && currentTrack?.id === track.id ? (
-                      <FaPause className="text-[#08B2F0]" />
-                    ) : (
-                      <>
-                        <span className="group-hover:hidden">{index + 1}</span>
-                        <FaPlay className="hidden group-hover:block absolute -top-2 text-white" />
-                      </>
+    <section className="px-6 pt-10 pb-36 w-full h-full">
+      {tracks.map((track, index) => {
+        return (
+          <div
+            key={`track-${track.id}`}
+            id={track.id}
+            className={cn(
+              "group hover:bg-neutral-700/50 rounded-lg transition-colors flex gap-2 py-2 px-3 [&:not(last-child)]:mb-2",
+              activeTrack === track.id && "border bg-neutral-700/50",
+            )}
+            onClick={() => toggleTrackSelection(track.id)}
+          >
+            {/* Serial Number, Play/pause btn */}
+            <div className="place-content-center place-items-center w-7 ">
+              <>
+                <span
+                  className={cn(
+                    "group-hover:hidden",
+                    activeTrack === track.id && "hidden",
+                  )}
+                >
+                  {index + 1}
+                </span>
+                {isPlaying && currentTrack?.id === track.id ? (
+                  <FaPause className="text-[#08B2F0]" />
+                ) : (
+                  <FaPlay
+                    className={cn(
+                      "hidden group-hover:inline-block text-white",
+                      activeTrack === track.id && "inline-block",
                     )}
-                  </div>
-                </td>
-                <td className="px-4 py-3">
-                  <div className="flex items-center gap-3">
-                    <div className="relative size-10">
-                      <img
-                        src={track.thumbnail}
-                        alt={track.title}
-                        className={`w-10 h-10 rounded ${
-                          isLoading && currentTrack?.id === track.id
-                            ? "opacity-50"
-                            : ""
-                        }`}
-                      />
-                      {isLoading && currentTrack?.id === track.id && (
-                        <div className="absolute inset-0 flex items-center justify-center bg-black/40 rounded">
-                          <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-white"></div>
-                        </div>
-                      )}
-                    </div>
-                    <div className="flex flex-col">
-                      <div
-                        className={`font-normal line-clamp-2  ${
-                          currentTrack?.id === track.id ? "text-[#08B2F0]" : ""
-                        }`}
-                      >
-                        {track.title}
-                      </div>
-                      <div className="text-sm text-neutral-400">
-                        {track.artist}
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td className="px-4 py-3 text-neutral-400">
-                  {track.likes || 0}
-                </td>
-                <td className="px-4 py-3 text-center">
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleDownloadTrack(track);
-                    }}
-                    className="bg-transparent text-neutral-400 hover:text-[#08B2F0] transition-colors"
-                  >
-                    <FaDownload size={16} />
-                  </button>
-                </td>
-                <td className="px-4 py-3 text-right text-neutral-400">
-                  {formatDuration(track.duration)}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+                  />
+                )}
+              </>
+            </div>
+            {/* Track Title */}
+            <div className="flex items-center justify-start gap-3 mr-auto max-w-3/5">
+              <div className="flex flex-col">
+                <p
+                  className={`font-normal line-clamp-1 text-ellipsis ${
+                    currentTrack?.id === track.id ? "text-highlight" : ""
+                  }`}
+                >
+                  {track.title}
+                </p>
+                <div className="text-sm text-neutral-400">{track.artist}</div>
+              </div>
+            </div>
+            {/* Like and dislike buttons */}
+            <div className="flex items-center justify-start gap-2 ml-auto">
+              <div className="size-6 relative group/likeBtn">
+                <FaRegThumbsUp
+                  size="20"
+                  className={cn(
+                    "absolute inset-0 z-5 group-hover/likeBtn:opacity-0 transition-opacity duration-200 fill-text-neutral-300 text-neutral-300",
+                    activeTrack === track.id &&
+                      "group-active/likeBtn:opacity-0",
+                  )}
+                  onClick={(e) => e.stopPropagation()}
+                />
+                <FaThumbsUp
+                  size="19"
+                  className={cn(
+                    "absolute inset-0 z-4 opacity-0 group-hover/likeBtn:opacity-100 transition-opacity duration-200 text-neutral-300 fill-neutral-300",
+                    activeTrack === track.id &&
+                      "group-active/likeBtn:opacity-100",
+                  )}
+                  onClick={(e) => e.stopPropagation()}
+                />
+              </div>
+              <div className="size-6 relative group/likeBtn">
+                <FaRegThumbsDown
+                  size="20"
+                  className={cn(
+                    "absolute inset-0 z-5 group-hover/likeBtn:opacity-0 transition-opacity duration-200 fill-text-neutral-300 text-neutral-300",
+                    activeTrack === track.id &&
+                      "group-active/likeBtn:opacity-0",
+                  )}
+                  onClick={(e) => e.stopPropagation()}
+                />
+                <FaThumbsDown
+                  size="19"
+                  className={cn(
+                    "absolute inset-0 z-4 opacity-0 group-hover/likeBtn:opacity-100 transition-opacity duration-200 text-neutral-300 fill-neutral-300",
+                    activeTrack === track.id &&
+                      "group-active/likeBtn:opacity-100",
+                  )}
+                  onClick={(e) => e.stopPropagation()}
+                />
+              </div>
+            </div>
+            {/* Track likes count and Download btns */}
+            <div className="px-4 py-3 text-neutral-400 hidden lg:flex lg:gap-1 lg:items-center">
+              <span>{track.likes || 0}</span>
+              <FaThumbsUp size="14" className="mb-[4px]" />
+            </div>
+            <div className="px-4 py-3 text-center hidden lg:block">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDownloadTrack(track);
+                }}
+                className="bg-transparent text-neutral-400 hover:text-[#08B2F0] transition-colors"
+              >
+                <FaDownload size={16} />
+              </button>
+            </div>
+            {/* Track Duration + sub menu btn */}
+            <div className="w-7 text-right text-neutral-400 h-full mt-auto mb-auto">
+              <span
+                className={cn(
+                  "group-hover:hidden",
+                  activeTrack === track.id && "hidden",
+                )}
+              >
+                {formatDuration(track.duration)}
+              </span>
+              <button
+                className={cn(
+                  "hidden group-hover:inline-block hover:bg-background rounded-full p-2",
+                  activeTrack === track.id &&
+                    "inline-block active:bg-background",
+                )}
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <EllipsisVerticalIcon size="20px" />
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </section>
   );
 }
