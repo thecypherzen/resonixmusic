403dd582088c6f8188bbecdcbb446808d1ba505f
William Inyam <sw.inyam@outlook.com>
2026-07-01 00:16:27 +0100

    :construction:wip:complete context menu comp w/o test

--- frontend/src/components/ContextMenu.jsx
+++ frontend/src/components/ContextMenu.jsx
@@ -1,33 +1,31 @@
+import { cn } from "@/lib/utils";
 import { button } from "framer-motion/client";
 import React, { createContext, useContext } from "react";
 
 const ContextMenuContext = createContext(null);
 
 // ContextMenu
-export function ContextMenu({ id, children }) {
+export function ContextMenu({ children }) {
   return (
-    <ContextMenuContext.Provider value={{}} id={id}>
+    <ContextMenuContext.Provider value={{}}>
       <div className="context-menu">{children}</div>
     </ContextMenuContext.Provider>
   );
 }
 
 //Menu Trigger
-export function ContextMenuTrigger({
-  targetId,
-  children,
-  anchorName,
-  className,
-  ...rest
-}) {
+export function ContextMenuTrigger({ target, children, className, ...rest }) {
+  const context = useContext(ContextMenuContext);
+  if (!!!context)
+    throw new Error("ContextMenuTrigger must be used inside a ContextMenu");
   return (
     <button
-      popovertarget={`${targetId}`}
+      popovertarget={`${target}`}
       className={className}
       onClick={(e) => {
         e.stopPropagation();
       }}
-      style={{ anchorName: `${anchorName}` }}
+      style={{ anchorName: `--anchor-for-${target}` }}
       {...rest}
     >
       {children}
@@ -35,10 +33,44 @@ export function ContextMenuTrigger({
   );
 }
 
+export function ContextMenuContent({
+  id,
+  children,
+  popover,
+  className,
+  ...props
+}) {
+  const context = useContext(ContextMenuContext);
+  if (!!!context)
+    throw new Error("ContextMenuContent must be used inside a ContextMenu");
+  return (
+    <aside
+      id={id}
+      {...props}
+      popover={popover ?? "auto"}
+      className={cn(
+        "border border-foreground/20 text-foreground min-h-16 inset-auto m-0 rounded-md absolute content-center bg-neutral-900",
+        className,
+      )}
+      style={{
+        positionAnchor: `--anchor-for-${id}`,
+        positionArea: "block-end span-inline-end",
+        positionTryFallbacks: "flip-block, flip-inline, normal",
+      }}
+    >
+      {children}
+    </aside>
+  );
+}
+
 // MenuItem
-export function ContextMenuItem({ children, ...props }) {
+export function ContextMenuItem({ children, className, ...props }) {
   const context = useContext(ContextMenuContext);
   if (!!!context)
     throw new Error("ContextMenuItem must be used inside a ContextMenu");
-  return <button {...props}>{children}</button>;
+  return (
+    <button className={cn("py-2 px-5", className)} {...props}>
+      {children}
+    </button>
+  );
 }
