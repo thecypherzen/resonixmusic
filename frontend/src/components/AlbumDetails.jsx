48750d74ec5cdc9d4ed5f5cd5bdd9b7a34e8d8f4
William Inyam <sw.inyam@outlook.com>
2026-06-30 06:28:46 +0100

    :bug:fix: resolve resolve single album display error

--- frontend/src/components/AlbumDetails.jsx
+++ frontend/src/components/AlbumDetails.jsx
@@ -1,20 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
-import {
-  FaPlay,
-  FaPause,
-  FaHeart,
-  FaEllipsisH,
-  FaClock,
-  FaDownload,
-} from "react-icons/fa";
 import { MdErrorOutline } from "react-icons/md";
 import { usePlayer } from "../context/PlayerContext";
 import api from "../services/api";
 import { saveAs } from "file-saver";
 import { LoadingState } from "./ContentSection";
 import TracksList from "./TracksList";
-import { DetailsPlayBtn } from "./DetailsPageHeader";
 
 const AlbumDetails = ({ id }) => {
   const player = usePlayer();
@@ -145,33 +135,22 @@ const AlbumDetails = ({ id }) => {
   return (
     <div className="flex-1 overflow-y-auto w-full">
       <div className="flex flex-col bg-transparent px-10 -mt-16">
-        <DetailsPageHeader
-          dataSet={album}
-          type="album"
-          tracksCount={tracks?.length}
-          namespace="albums"
-        />
+        <p>
+          Use <span>SingleAlbumPage</span>&nbsp;instead
+        </p>
         {/* Tracks List */}
-        <TracksList tracks={tracks} />
+        {/*<TracksList
+          tracks={tracks}
+          artistPerTrack={false}
+          className="border border-red-500"
+        />*/}
       </div>
     </div>
   );
 };
 
 const ErrorMessage = ({ message }) => (
-  <div className="flex justify-center items-center h-[75vh] w-[60rem] mx-16 fixed">
-    <div className="text-neutral-600 flex flex-col items-center">
-      <MdErrorOutline size={102} className="m-auto" />
-      <p className="text-2xl mb-2 font-extrabold">Unable to load content</p>
-      <p className="text-sm">{message}</p>
-      <button
-        onClick={() => window.location.reload()}
-        className="text-sm mt-4 px-8 py-2 bg-transparent border rounded-full border-neutral-700 hover:bg-neutral-800 transition-all duration-200"
-      >
-        Retry
-      </button>
-    </div>
-  </div>
+  <div className="flex justify-center items-center h-[75vh] w-[60rem] mx-16 fixed"></div>
 );
 
 export default AlbumDetails;
--- frontend/src/pages/SingleAlbumPage.jsx
+++ frontend/src/pages/SingleAlbumPage.jsx
@@ -70,21 +70,42 @@ const SingleAlbumPage = () => {
     }
   }, [albumTracks]);
 
-  if (isLoading) return <LoadingState />;
-  if (error) return <div>{error?.message ?? "A loading error occured"}</div>;
+  if (error) return <ErrorMessage message={error} />;
+  if (!selectedAlbum) return <div>Album not found</div>;
+  if (isLoading) return <LoadingState component="artist-details" />;
+
   return (
-    <div className="flex-1 w-full min-h-[calc(100vh-3.5rem-26px)]">
-      <div className="flex flex-col">
-        <DetailsPageHeader
-          type="album"
-          dataSet={selectedAlbum}
-          tracksCount={albumTracks.length ?? 0}
-          namespace="albums"
-        />
-        <TracksList tracks={albumTracks} />
-      </div>
-    </div>
+    <main className="flex flex-col flex-1 overflow-y-auto w-full ">
+      <DetailsPageHeader
+        dataSet={selectedAlbum}
+        type="album"
+        tracksCount={albumTracks?.length}
+        namespace="albums"
+      />
+      {/* Tracks List */}
+      <TracksList
+        tracks={albumTracks}
+        artistPerTrack={false}
+        className="px-10"
+      />
+    </main>
   );
 };
+
+const ErrorMessage = ({ message }) => (
+  <div className="flex justify-center items-center h-[75vh] w-[60rem] mx-16 fixed">
+    <div className="text-neutral-600 flex flex-col items-center">
+      <MdErrorOutline size={102} className="m-auto" />
+      <p className="text-2xl mb-2 font-extrabold">Unable to load content</p>
+      <p className="text-sm">{message}</p>
+      <button
+        onClick={() => window.location.reload()}
+        className="text-sm mt-4 px-8 py-2 bg-transparent border rounded-full border-neutral-700 hover:bg-neutral-800 transition-all duration-200"
+      >
+        Retry
+      </button>
+    </div>
+  </div>
+);
 
 export default SingleAlbumPage;
